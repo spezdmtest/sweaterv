2077c5c1c331ced1b42f177857d03e28a6708031
spezdmtest <spezd.m.@gmail.com>
2021-05-30 10:18:56 +0300

    sweater

---
+++ src/main/java/com/example/sweaterv/GreetingController.java
@@ -0,0 +1,25 @@
+package com.example.sweaterv;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam(name="name", required = false, defaultValue = "World") String name,
+            Model model
+    ) {
+        model.addAttribute("name",name);
+        return "greeting";
+    }
+    @GetMapping
+    public String main(Model model){
+        model.addAttribute("some","hello, testCode!");
+        return "main";
+    }
+}
